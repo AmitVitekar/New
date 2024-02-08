b92efc34be8501aebc31d8876617a29faccfe83d
AmitVitekar <va.vitekaramit@gmail.com>
2024-02-08 21:56:35 +0530

    Initial commit

---
+++ Assigmnet/Assignment2.py
@@ -0,0 +1,24 @@
+# Develop a Python script that calculates the square and cube of a given number.
+
+A = int( input ("Please Enter Number : "))
+B = int (input ("Please Enter the power: "))
+
+print (f"Cube of {A} is :", A**B)
+
+# Create a program that takes two numbers as input and prints whether
+# the first number is greater than, less than, or equal to the second number.
+
+a = 5
+b = 6
+
+print (a >= b)
+print (a == b)
+print (a <= b)
+
+
+# Write a Python program to calculate the area of a circle given its radius using the formula
+# area=π×r^2 ( Take pie as 3.14)
+
+a = int(input("Enter the circle r :"))
+
+print ("area of circle is :", (3.14 * (a**2)))
